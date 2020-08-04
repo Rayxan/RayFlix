345f7c70396faa2dfc889519a89ae18815c0e916
Raylan Sales <raylanwork@gmail.com>
2020-08-04 20:25:30 -0300

    Configurado deploy no heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,9 +28,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    console.log('asdasdsa papapap');
-
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://rayflix.herokuapp.com/categorias';
 
     fetch(URL_TOP)
       .then(async (respostaDoServidor) =>{
